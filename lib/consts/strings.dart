b898b3b518faacbe429afc08e0106924d714060c
Your Name <your.email@example.com>
2024-09-03 00:51:41 +0500

    first commit

---
+++ lib/consts/strings.dart
@@ -0,0 +1,6 @@
+const String degree = "°";
+const String apiKey = "863483454d50f898d63179799ed14e46";
+const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const String lat='	31.582045';
+const String long='74.329376';
